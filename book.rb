cf4e57f0fdb1ce17b4d8ad148c9e7cdece0b1ff4
Lul-Abdifan <tasanbannu@gmail.com>
2023-07-12 08:56:49 -0400

    Fix eslint error

--- book.rb
+++ book.rb
@@ -1,16 +1,15 @@
 require_relative 'item'
 
-class Book <Item
-    attr_accessor: publisher,cover_state
+class Book < Item
+  attr_accessor :publisher, :cover_state
 
-def initialize(publisher,cover_state,     publish_date)
+  def initialize(publisher, cover_state, publish_date)
     super(publish_date)
-    @publisher  = publisher
+    @publisher = publisher
     @cover_state = cover_state
-end
+  end
 
-def can_be_archived?
-    super || cover_state == "bad"
+  def can_be_archived?
+    super || cover_state == 'bad'
+  end
 end
-
-end 
