c227ef94126c0937b56219be220fcc529a87c210
Lul-Abdifan <tasanbannu@gmail.com>
2023-07-12 04:28:48 -0400

    Fix eslint error

--- item.rb
+++ item.rb
@@ -1,27 +1,26 @@
 class Item
-    attr_accessor :genre, :author, :source, :label, :publish_date, :publish_date
-    attr_reader :id, :archived, 
+  attr_accessor :genre, :author, :source, :label, :publish_date
+  attr_reader :id, :archived
 
-    def initialize(genre, author, source,label,publish_date,archived)
-        @id =Random.rand(0..1000)
-        @genre = genre
-        @author = author
-        @source = source
-        @label = label
-        @publish_date = publish_date
-        @archived = archived
-    end
+  def initialize(params)
+    @id = Random.rand(0..1000)
+    @genre = params[:genre]
+    @author = params[:author]
+    @source = params[:source]
+    @label = params[:label]
+    @publish_date = params[:publish_date]
+    @archived = params[:archived]
+  end
 
-    def move_to_archive()
-        if(can_be_archived?())
-            @archived = true           
-        end        
-    end
+  def move_to_archive()
+    return unless can_be_archived?
 
-    private
-    def can_be_archived?()
-        @publish_date>10
-    end
- 
+    @archived = true
+  end
 
+  private
+
+  def can_be_archived?()
+    @publish_date > 10
+  end
 end
