b95bc209796e4aff24807b1ecd118ec3a09615b0
Adams-Ghub <bamfoadamsf@gmail.com>
2023-07-13 15:12:38 +0000

    Create the option.rb file

---
+++ options.rb
@@ -0,0 +1,11 @@
+class Options
+  def choices
+    puts 'WELCOME TO CATALOG OF MY THINGS APP'
+    puts 'Choose an option by entering its number'
+    puts '1. List all games         2. List all books           3. List all music albums'
+    puts '4. List all movies        5. List all genres          6. List all labels '
+    puts '7. List all authors       8. List all sources         9. Add a book'
+    puts '10. Add an author         11. Add a music album       12. Add a movie'
+    puts '13. Add a game'
+  end
+end
