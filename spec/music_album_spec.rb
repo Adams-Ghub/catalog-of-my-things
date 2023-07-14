071015d0d271eee6af7b495e2c2a912b3c2ed7f0
IRADUKUNDA Pacific Rugwizangoga <pacific.r.iradukunda@gmail.com>
2023-07-14 19:07:05 +0200

    Update music_album_spec.rb

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -4,17 +4,17 @@ require_relative '../music_album'
 
 describe 'Game' do
   before :all do
-    @myMusic = MusicAlbum.new('2016-09-07', true)
+    @my_music = MusicAlbum.new('2016-09-07', true)
     @music = MusicAlbum.new('2010-08-03', false)
     @musical = MusicAlbum.new('2009-09-07', true)
   end
 
   context 'cannot be archived if' do
     it 'if parent\'s method returns true AND if on_spotify equals true' do
-      expect(@myMusic.can_be_archived?).to be false
+      expect(@my_music.can_be_archived?).to be false
     end
     it 'if parent\'s method returns true AND if on_spotify equals true' do
-      expect(@myMusic.can_be_archived?).to be false
+      expect(@music.can_be_archived?).to be false
     end
   end
 
