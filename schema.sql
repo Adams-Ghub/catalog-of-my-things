4fc2fadce21e5a7146471e21119b234ad2b07af4
Lul-Abdifan <tasanbannu@gmail.com>
2023-07-14 14:44:40 -0400

    Merge schema files

--- schema.sql
+++ schema.sql
@@ -52,3 +52,22 @@ CREATE TABLE Author (
     last_name VARCHAR(100)
 )
 
+-- Table: music_albums
+CREATE TABLE music_albums (
+  id SERIAL PRIMARY KEY,
+  publish_date DATE NOT NULL,
+  archived BOOLEAN NOT NULL,
+  on_spotify BOOLEAN NOT NULL,
+
+  author_id INTEGER REFERENCES authors (id),
+  genre_id INTEGER REFERENCES genres (id),
+  source_id INTEGER REFERENCES sources(id),
+  label_id INTEGER REFERENCES labels (id)
+);
+
+-- Table: genres
+CREATE TABLE genres (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
