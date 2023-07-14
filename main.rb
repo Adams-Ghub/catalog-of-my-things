ba379b136cef3a9b4bd7bcb3659204d53cdd0b80
Lul-Abdifan <tasanbannu@gmail.com>
2023-07-14 03:50:56 -0400

    Update the break functionality to save the data

--- main.rb
+++ main.rb
@@ -2,14 +2,15 @@ require_relative 'app'
 require_relative 'options'
 def main
   app = App.new
+  app.load
+  
   options = Options.new
 
   loop do
     options.choices
     puts 'Enter an option'
     input = gets.chomp.to_i
-    break if input.zero? || input > 14
-
+    break app.save puts if input == 0
     app.executors(input)
   end
 end
